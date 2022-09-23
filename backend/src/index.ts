da9f88380b5bc87fe57e23a06238b5255bdf537a
Nelsen Septa <nelsenseptahenidar@gmail.com>
2022-09-23 23:13:36 +0700

    connect mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello Word");
+import mongoose from "mongoose";
+import express, { application } from "express";
+import cors from "cors";
+import passport from "passport";
+import passportLocal from "passport-local";
+import cookieParser from "express-session";
+import bcrypt from "bcryptjs";
+import session from "express-session";
+import dotent from "dotenv";
+
+mongoose.connect(
+  "server",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err: Error) => {
+    if (err) throw err;
+    console.log("Connected To Mongo");
+  }
+);
+
+// middleware
